5a97d4240d4adf1ee638f2a0190997aed20ea284
Ahmed Ahmed <ahmedahmed@Ahmeds-MacBook-Air.local>
2022-10-22 02:34:39 -0400

    First commit

---
+++ my-app/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, {
+  useState
+} from "react";
+import ButtonAppBar from "./Appbar";
+import Footer from "./Footer";
+
+
+
+
+function App() {
+
+  return (
+    <div>
+      <ButtonAppBar/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
